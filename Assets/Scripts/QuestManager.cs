23786939c7d8aa1568be044c481a516f0cc59434
JoaoPauloLisboa92 <joaopaulo@pillow.ac>
2017-06-24 23:16:04 -0300

    implementação sistema de dinheiro

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestManager : MonoBehaviour
 {
@@ -9,15 +10,20 @@ public class QuestManager : MonoBehaviour
     public string itemCollected;
     public bool itemColetado = false;
 
+	//informação do dinheiro
+	public GameObject moneySystem;
+	public moneySystem dados;
+
 	// Use this for initialization
 	void Start ()
     {
         questCompleted = new bool[quests.Length];
+		dados = moneySystem.GetComponent<moneySystem>();
+
 	}
 	
 	// Update is called once per frame
-	void Update ()
-    {
+	void Update (){
 		
 	}
 
@@ -27,5 +33,6 @@ public class QuestManager : MonoBehaviour
         theDM.dialogLines[0] = questText;
         theDM.currentLine = 0;
         theDM.ShowDialogue();
+		dados.addMoney (500);
     }
 }
---
+++ Assets/Scripts/moneySystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moneySystem : MonoBehaviour {
+	public int money;
+	public Text displayedMoney;
+	// Use this for initialization
+	void Start () {
+		money = 100;
+		displayedMoney.text = money.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+
+	public void addMoney(int moneyToAdd){
+		money += moneyToAdd;
+		displayedMoney.text = money.ToString ();
+	}
+
+	public void subtractMoney(int moneyToSubtract){
+		if(money - moneyToSubtract < 0){
+			Debug.Log ("Sem dinheiro suficiente");
+		}else{
+			money -= moneyToSubtract;
+			displayedMoney.text = money.ToString ();
+		}
+	}
+}
